94093a28c1bc49f2c7622a83e27012d11a2d9ecb
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-08-08 19:12:02 +1200

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace TimeTraveler;
 public static class ModHelperData
 {
     public const string Name = "Time Traveler";
-    public const string Description = "The Time Traveler from the past, present and future";
+    public const string Description = "The Time traveler is here, 15 upgrades so you can see the, past, future, and present. Also comes with custom models.";
     public const string Version = "2.0.0";
     public const string RepoOwner = "Commander-Cat101";
     public const string RepoName = "TimeTraveler";
