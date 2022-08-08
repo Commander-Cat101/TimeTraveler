0ed128b8e39029b18a75060ce20f03712d33d2ed
Commander-Cat101 <102434202+Commander-Cat101@users.noreply.github.com>
2022-08-08 16:35:13 +1200

    Create ModHelperData.cs

---
+++ ModHelperData.cs
@@ -0,0 +1,10 @@
+namespace Round_Changer;
+
+public static class ModHelperData
+{
+    public const string Name = "Time Traveler";
+    public const string Description = "The Time Traveler from the past, present and future";
+    public const string Version = "2.0.0";
+    public const string RepoOwner = "Commander-Cat101";
+    public const string RepoName = "TimeTraveler";
+}
